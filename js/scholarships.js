040be1016d8a31037aff88131e5d8e7c2c66ad32
Phil Cable <pcable@wsu.edu>
2016-07-25 13:46:07 -0700

    Rudimentary filtering

--- js/scholarships.js
+++ js/scholarships.js
@@ -17,11 +17,11 @@
 				resident: $('[name=wsuwp-scholarship-resident]:checked').val()
 			};
 
-		$.post( scholarships.ajax_url, data, function (response) {
+		$.post(scholarships.ajax_url, data, function (response) {
 			$('.wsuwp-scholarships-loading').after('<p>Based on the information you entered, you should be eligible for the following scholarships. Some may have additional requirements, so please be sure to read the details for a scholarship before applying.</p>').remove();
 			$('.wsuwp-scholarships-filters').show();
 			$('.wsuwp-scholarships').html(response);
-		} );
+		});
 	});
 
 	// Sort scholarships.
@@ -29,7 +29,7 @@
 		var scholarships = $('.wsuwp-scholarships article'),
 			selected = $(this).val();
 
-		scholarships.sort(function (a,b) {
+		scholarships.sort(function (a, b) {
 			var an = a.getAttribute('data-' + selected),
 				bn = b.getAttribute('data-' + selected);
 
@@ -37,5 +37,22 @@
 		});
 
 		scholarships.detach().appendTo($('.wsuwp-scholarships'));
+	});
+
+	// Show/hide scholarships.
+	$('.wsuwp-scholarship-eligibility').on('change', 'input:checkbox', function () {
+		var scholarships = $('.wsuwp-scholarships article'),
+			selected = [];
+
+		$('.wsuwp-scholarship-eligibility input:checkbox:checked').each(function () {
+			selected.push('.eligibility-' + $(this).val());
+		});
+
+		if (selected.length > 0) {
+			scholarships.not(selected.join(',')).hide('fast');
+			scholarships.filter(selected.join(',')).show('fast');
+		} else {
+			scholarships.show('fast');
+		}
 	});
 }(jQuery));
