0335ba94c5243bb4eaa9ef1a1fe4a8aebce8ee07
Phil Cable <pcable@wsu.edu>
2016-12-22 15:25:30 -0800

    Bump version 0.0.4

--- wsuwp-scholarships.php
+++ wsuwp-scholarships.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSU Scholarships
-Version: 0.0.3
+Version: 0.0.4
 Description: Provides a content type for publishing and managing a collection of scholarships.
 Author: washingtonstateuniversity, philcable
 Author URI: https://web.wsu.edu/
