a62ae6e4d4d16550b773a21a3fb4def747aad568
Phil Cable <pcable@wsu.edu>
2017-01-23 14:54:03 -0800

    Include PostCSS and CSSLint in Grunt processing

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,65 @@ module.exports = function( grunt ) {
     grunt.initConfig( {
         pkg: grunt.file.readJSON( "package.json" ),
 
+        copy: {
+            style: {
+                expand: true,
+                cwd: "src/css/",
+                src: "*",
+                dest: "css/",
+                filter: "isFile"
+            }
+        },
+
+        postcss: {
+            options: {
+                processors: [
+                    require( "autoprefixer" )( {
+                        browsers: [ "> 1%", "ie 8-11", "Firefox ESR" ]
+                    } )
+                ]
+            },
+            dist: {
+                src: "css/*.css"
+            }
+        },
+
+        csslint: {
+            main: {
+                src: [ "css/*.css" ],
+                options: {
+                    "fallback-colors": false,            // Unless we want to support IE8
+                    "box-sizing": false,                 // Unless we want to support IE7
+                    "compatible-vendor-prefixes": false, // The library on this is older than autoprefixer.
+                    "gradients": false,                  // This also applies ^
+                    "overqualified-elements": false,     // We have weird uses that will always generate warnings.
+                    "ids": false,
+                    "regex-selectors": false,            // Audit
+                    "adjoining-classes": false,
+                    "box-model": false,                  // Audit
+                    "universal-selector": false,         // Audit
+                    "unique-headings": false,            // Audit
+                    "outline-none": false,               // Audit
+                    "floats": false,
+                    "font-sizes": false,                 // Audit
+                    "important": false,                  // This should be set to 2 one day.
+                    "unqualified-attributes": false,     // Should probably be 2 one day.
+                    "qualified-headings": false,
+                    "known-properties": 1,               // Okay to ignore in the case of known unknowns.
+                    "duplicate-background-images": 2,
+                    "duplicate-properties": 2,
+                    "star-property-hack": 2,
+                    "text-indent": 2,
+                    "display-property-grouping": 2,
+                    "shorthand": 2,
+                    "empty-rules": 2,
+                    "vendor-prefix": 2,
+                    "zero-units": 2,
+                    "order-alphabetical": false
+                }
+            }
+        },
+
         phpcs: {
             plugin: {
                 src: [ "./*.php", "./includes/*.php" ]
@@ -25,7 +84,7 @@ module.exports = function( grunt ) {
 
         jshint: {
             grunt_script: {
-                src: [ "Gruntfile.js" ],
+                src: [ "Gruntfile.js", "js/*.js" ],
                 options: {
                     curly: true,
                     eqeqeq: true,
@@ -39,10 +98,13 @@ module.exports = function( grunt ) {
         }
     } );
 
-    grunt.loadNpmTasks( "grunt-jscs" );
+    grunt.loadNpmTasks( "grunt-contrib-csslint" );
+    grunt.loadNpmTasks( "grunt-contrib-copy" );
     grunt.loadNpmTasks( "grunt-contrib-jshint" );
+    grunt.loadNpmTasks( "grunt-jscs" );
     grunt.loadNpmTasks( "grunt-phpcs" );
+    grunt.loadNpmTasks( "grunt-postcss" );
 
     // Default task(s).
-    grunt.registerTask( "default", [ "phpcs", "jscs", "jshint" ] );
+    grunt.registerTask( "default", [ "copy", "postcss", "csslint", "phpcs", "jscs", "jshint" ] );
 };
