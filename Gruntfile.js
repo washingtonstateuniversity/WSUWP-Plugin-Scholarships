da9f061f4953525e501945a063b5a97c9adf9362
Phil Cable <pcable@wsu.edu>
2017-06-30 15:54:07 -0700

    Update Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,9 @@
-var Promise = require( "es6-promise" ).polyfill();
-
 module.exports = function( grunt ) {
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( "package.json" ),
 
-		copy: {
-			style: {
-				expand: true,
-				cwd: "src/css/",
-				src: "*",
-				dest: "css/",
-				filter: "isFile"
-			}
+		stylelint: {
+			src: "src/css/*.css"
 		},
 
 		postcss: {
@@ -23,43 +15,10 @@ module.exports = function( grunt ) {
 				]
 			},
 			dist: {
-				src: "css/*.css"
-			}
-		},
-
-		csslint: {
-			main: {
-				src: [ "css/*.css" ],
-				options: {
-					"fallback-colors": false,            // Unless we want to support IE8
-					"box-sizing": false,                 // Unless we want to support IE7
-					"compatible-vendor-prefixes": false, // The library on this is older than autoprefixer.
-					"gradients": false,                  // This also applies ^
-					"overqualified-elements": false,     // We have weird uses that will always generate warnings.
-					"ids": false,
-					"regex-selectors": false,            // Audit
-					"adjoining-classes": false,
-					"box-model": false,                  // Audit
-					"universal-selector": false,         // Audit
-					"unique-headings": false,            // Audit
-					"outline-none": false,               // Audit
-					"floats": false,
-					"font-sizes": false,                 // Audit
-					"important": false,                  // This should be set to 2 one day.
-					"unqualified-attributes": false,     // Should probably be 2 one day.
-					"qualified-headings": false,
-					"known-properties": 1,               // Okay to ignore in the case of known unknowns.
-					"duplicate-background-images": 2,
-					"duplicate-properties": 2,
-					"star-property-hack": 2,
-					"text-indent": 2,
-					"display-property-grouping": 2,
-					"shorthand": 2,
-					"empty-rules": 2,
-					"vendor-prefix": 2,
-					"zero-units": 2,
-					"order-alphabetical": false
-				}
+				cwd: "src/css/",
+				src: "*.css",
+				dest: "css/",
+				expand: true
 			}
 		},
 
@@ -117,13 +76,12 @@ module.exports = function( grunt ) {
 		}
 	} );
 
-	grunt.loadNpmTasks( "grunt-contrib-csslint" );
-	grunt.loadNpmTasks( "grunt-contrib-copy" );
 	grunt.loadNpmTasks( "grunt-contrib-jshint" );
 	grunt.loadNpmTasks( "grunt-jscs" );
 	grunt.loadNpmTasks( "grunt-phpcs" );
 	grunt.loadNpmTasks( "grunt-postcss" );
+	grunt.loadNpmTasks( "grunt-stylelint" );
 
 	// Default task(s).
-	grunt.registerTask( "default", [ "copy", "postcss", "csslint", "phpcs", "jscs", "jshint" ] );
+	grunt.registerTask( "default", [ "stylelint", "postcss", "jscs", "jshint", "phpcs" ] );
 };
