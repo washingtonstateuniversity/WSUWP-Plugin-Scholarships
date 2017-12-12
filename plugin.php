73e6ce13ddf225e39223ba19dbd835683dfe20a5
Phil Cable <pcable@wsu.edu>
2017-12-12 09:36:01 -0800

    Address PHPCS error

--- plugin.php
+++ plugin.php
@@ -18,8 +18,9 @@ if ( ! defined( 'WPINC' ) ) {
 
 // This plugin uses namespaces and requires PHP 5.3 or greater.
 if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
-	add_action( 'admin_notices', create_function( '',
-	"echo '<div class=\"error\"><p>" . __( 'WSU Scholarships requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsu-scholarships' ) . "</p></div>';" ) );
+	add_action( 'admin_notices', function() {
+		echo '<div class="error"><p>' . esc_html__( 'WSU Scholarships requires PHP 5.3 to function properly. Please upgrade PHP or deactivate the plugin.', 'wsu-scholarships' ) . '</p></div>';
+	} );
 	return;
 } else {
 	add_action( 'plugins_loaded', 'WSU\Scholarships\bootstrap' );
@@ -41,9 +42,9 @@ if ( version_compare( PHP_VERSION, '5.3', '<' ) ) {
 	 * @since 0.1.0
 	 */
 	function bootstrap() {
-		include_once dirname( __FILE__ ) . '/includes/scholarship-post-type.php';
-		include_once dirname( __FILE__ ) . '/includes/scholarship-settings.php';
-		include_once dirname( __FILE__ ) . '/includes/scholarship-shortcodes.php';
-		include_once dirname( __FILE__ ) . '/includes/scholarship-contributor-role.php';
+		include_once __DIR__ . '/includes/scholarship-post-type.php';
+		include_once __DIR__ . '/includes/scholarship-settings.php';
+		include_once __DIR__ . '/includes/scholarship-shortcodes.php';
+		include_once __DIR__ . '/includes/scholarship-contributor-role.php';
 	}
 }
