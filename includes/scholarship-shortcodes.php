6960fb040f91789aaa0e5515f6c0004a1aa54844
Phil Cable <pcable@wsu.edu>
2018-04-17 08:43:12 -0700

    Add the spirit mark as an indicator of scholarships provided by WSU

--- includes/scholarship-shortcodes.php
+++ includes/scholarship-shortcodes.php
@@ -228,6 +228,15 @@ function display_wsuwp_scholarships() {
 	<div class="wsuwp-scholarships-tools">
 		<a class="back-to-top" title="Back to top" href="#">Back to top</a>
 	</div>
+
+	<svg id="scholarship-svg-symbols" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		<defs>
+			<symbol id="icon-wsu-scholarship" viewBox="0 0 44 44">
+				<path fill="#a60f2d" d="M26.3 43.4s2.2-1 3.5-4.6a9 9 0 0 1 .7 4.5 28.6 28.6 0 0 1-4.2 0zm9.3-7.3C29 37 28 23.4 28 23.4s2.2 7 6.8 6.7c4.7-.2 3.4-7.6 3.4-7.6s4.6 12.6-2.7 13.6zM6 33a56 56 0 0 1-6.2 1.5S3.5 31.7 6.2 23L9 25.5 8.4 27a12.3 12.3 0 0 1 1.6 3 7.9 7.9 0 0 0-.2-7l-.4 1-1-.9-1.7-1.6a22.3 22.3 0 0 1 3.3-6.2l.2.2 2 2.4-.6 1a26 26 0 0 1 2.6 3.3A15 15 0 0 0 14 16l-1 .9-2.2-2.7a25 25 0 0 1 10-6.4 7.4 7.4 0 0 0-.6.9c-1.3 1.7-2.6 5-1.5 10.1l.7 3.2c.5 2.4 1.2 5 1.4 6.8.4 3.5 0 5.8-1.1 7a5 5 0 0 1-4 1.2v-.8a18.2 18.2 0 0 0-.5-4.5l-.5-1.7-.8 1.6c-1.2 2.5-5.3 8.8-10.7 10A18 18 0 0 0 6 33z"/>
+				<path fill="#a60f2d" d="M36.9 9.7v-.3L43 8v-.6l-6.5.8v-.4l5.7-2.3L42 5l-6.1 1.9a2.9 2.9 0 0 0-1.2-1.2c-2.3.2-4.4.3-6.3.6L29.6.6H29l-1.9 5.9-1 .1L27.6.2h-.7L24.8 7h-.1a6.8 6.8 0 0 0-3.4 2.6C20.2 11 19 13.9 20 18.7l.7 3.1c.6 2.4 1.2 5.1 1.4 6.9.5 4 0 6.6-1.4 8.1-1.3 1.3-3.1 1.8-5.7 1.6h-.7v-.7a13.4 13.4 0 0 0 .1-1.4 17.8 17.8 0 0 0-.1-2.4 23.2 23.2 0 0 1-7.5 7.8A110.3 110.3 0 0 1 20.5 43l.7.1h.3a10.7 10.7 0 0 0 1.6 0h.3c3.2-.6 6-3 5.2-8.4-.9-6.5-2-10.6-2.3-13.7-.4-3.8 3-9.7 8.7-7a9.3 9.3 0 0 1 2 3.1 8.7 8.7 0 0 0-.3-4A6.7 6.7 0 0 0 37 11l6.4-.3V10zm-6.5 2.4a9.2 9.2 0 0 0-5.5 1.4 7.3 7.3 0 0 0-2 2.4 4 4 0 0 1 1.6-3.9 8.3 8.3 0 0 1 6.3-.3c.6.3.2.4-.4.4z"/>
+			</symbol>
+		</defs>
+	</svg>
 	<?php
 	$html = ob_get_contents();
 
@@ -484,7 +493,14 @@ function ajax_callback() {
 			$classes = implode( get_post_class( $meta_classes ), ' ' );
 
 			$post = '<article class="' . esc_attr( $classes ) . '" data-scholarship="' . esc_attr( $i ) . '" data-amount="' . esc_attr( $amount_data_value ) . '" data-deadline="' . esc_attr( $deadline_data_value ) . '">';
-			$post .= '<header class="name"><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></header>';
+			$post .= '<header class="name">';
+			if ( has_tag( 'wsu' ) ) {
+				$post .= '<svg xmlns="http://www.w3.org/2000/svg" width="23" height="23">';
+				$post .= '<title>WSU Scholarship</title>';
+				$post .= '<use xlink:href="#icon-wsu-scholarship" />';
+				$post .= '</svg> ';
+			}
+			$post .= '<a href="' . get_the_permalink() . '">' . get_the_title() . '</a></header>';
 			$post .= '<div class="amount">';
 
 			if ( $amount ) {
